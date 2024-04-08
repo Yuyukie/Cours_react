c08b55842c3b117b3e9708f063b1b950390ac0c0
Benjamin Mazars <mazars.benjamin@hotmail.fr>
2024-04-08 13:35:35 +0200

    mofification cours p2c4

--- la-maison-jungle/src/components/ShoppingList.js
+++ la-maison-jungle/src/components/ShoppingList.js
@@ -1,43 +1,30 @@
 import { plantList } from '../datas/plantList'
-import CareScale from './CareScale'
+import PlantItem from './PlantItem'
 import '../styles/ShoppingList.css'
 
-// Définit une fonction composant React nommée ShoppingList
 function ShoppingList() {
-	// Réduit la liste de plantes pour obtenir les catégories uniques
 	const categories = plantList.reduce(
-		// Utilise reduce pour créer un tableau d'accumulateur acc, ajoutant chaque catégorie unique à acc
 		(acc, plant) =>
-			// Vérifie si la catégorie de la plante est déjà dans l'accumulateur, si non, l'ajoute
 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
-		// Initialise l'accumulateur avec un tableau vide
 		[]
 	)
 
-	// Renvoie un élément JSX représentant la liste de courses
 	return (
 		<div>
-			{/* Affiche une liste non ordonnée des catégories */}
 			<ul>
-				{/* Mappe à travers les catégories et affiche chaque catégorie comme un élément de liste */}
 				{categories.map((cat) => (
 					<li key={cat}>{cat}</li>
 				))}
 			</ul>
-			{/* Affiche une liste ordonnée de plantes */}
 			<ul className='lmj-plant-list'>
-				{/* Mappe à travers la liste de plantes et affiche chaque plante comme un élément de liste */}
-				{plantList.map((plant) => (
-					<li key={plant.id} className='lmj-plant-item'>
-						{/* Affiche une icône de feu si la plante est en meilleure vente */}
-						{plant.isBestSale && <span>🔥</span>}
-						{/* Affiche le nom de la plante */}
-						{plant.name}
-						{/* Affiche l'échelle de soin pour l'arrosage de la plante */}
-						<CareScale careType='water' scaleValue={plant.water} />
-						{/* Affiche l'échelle de soin pour la lumière de la plante */}
-						<CareScale careType='light' scaleValue={plant.light} />
-					</li>
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<PlantItem
+						id={id}
+						cover={cover}
+						name={name}
+						water={water}
+						light={light}
+					/>
 				))}
 			</ul>
 		</div>
