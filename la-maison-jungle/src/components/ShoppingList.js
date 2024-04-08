2af75c8551229c3dbfa7624a6fb97791d88979d5
Benjamin Mazars <mazars.benjamin@hotmail.fr>
2024-04-08 11:54:47 +0200

    mise a jour exercice p2c4 begin

--- la-maison-jungle/src/components/ShoppingList.js
+++ la-maison-jungle/src/components/ShoppingList.js
@@ -1,34 +1,33 @@
-import { plantList } from '../datas/plantList';
-import '../styles/ShoppingList.css';
+import { plantList } from '../datas/plantList'
+import CareScale from './CareScale'
+import '../styles/ShoppingList.css'
 
 function ShoppingList() {
-    // Extraction des catégories uniques de la liste de plantes
-    const categories = [];
-    plantList.forEach((plant) => {
-        if (!categories.includes(plant.category)) {
-            categories.push(plant.category);
-        }
-    });
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
 
-    return (
-        <div>
-            {/* Affichage des catégories sous forme de liste */}
-            <ul>
-                {categories.map((cat) => (
-                    <li key={cat}>{cat}</li>
-                ))}
-            </ul>
-            {/* Affichage de la liste de plantes */}
-            <ul className='lmj-plant-list'>
-                {plantList.map((plant) => (
-                    <li key={plant.id} className='lmj-plant-item'>
-                        {plant.name}
-                        {plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+	return (
+		<div>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul className='lmj-plant-list'>
+				{plantList.map((plant) => (
+					<li key={plant.id} className='lmj-plant-item'>
+						{plant.isBestSale && <span>🔥</span>}
+						{plant.name}
+						<CareScale careType='water' scaleValue={plant.water} />
+						<CareScale careType='light' scaleValue={plant.light} />
+					</li>
+				))}
+			</ul>
+		</div>
+	)
 }
 
-export default ShoppingList;
+export default ShoppingList
